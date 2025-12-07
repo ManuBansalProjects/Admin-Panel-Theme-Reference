7274f32882eff156d4c0412bbbf110e2676b848d
pramod_jangid <pramod_jangid>
2025-12-07 18:59:35 +0530

    commit

--- build/static/js/606.fe3244d4.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreloaded_astrology=self.webpackChunkreloaded_astrology||[]).push([[606],{1606:(e,s,a)=>{a.r(s),a.d(s,{default:()=>l});const l={}}}]);
-//# sourceMappingURL=606.fe3244d4.chunk.js.map
